dbbfc8ba670b10afc7cb86647681b86fb205d28e
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-17 00:41:36 +0530

    :heavy_plus_sign: Add googleapis for types

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-export const nodeTs = () => true;
+import { readFileSync } from "fs";
+import { docs_v1 } from "googleapis";
+
+export const googleDocsToMarkdown = (file: docs_v1.Schema$Document) => {
+  let text = `---
+title: ${file.title}
+documentId: ${file.documentId}
+revisionId: ${file.revisionId}
+---
+
+`;
+  return text;
+};
+
+const file = JSON.parse(readFileSync("helpers/document.json").toString());
+console.log(googleDocsToMarkdown(file));
