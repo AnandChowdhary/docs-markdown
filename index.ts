95cf1ace21ea74df8b26f36073312ba78d49dbb1
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-28 15:37:11 +0530

    :sparkles: Fetch files from Google

--- index.ts
+++ index.ts
@@ -1,4 +1,38 @@
-import { docs_v1 } from "googleapis";
+import { google, docs_v1 } from "googleapis";
+import { join } from "path";
+import _fs from "fs";
+const fs = _fs.promises;
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_DOCS_CLIENT_ID,
+  process.env.GOOGLE_DOCS_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({
+  access_token: process.env.GOOGLE_DOCS_ACCESS,
+  refresh_token: process.env.GOOGLE_DOCS_REFRESH,
+});
+const docs = google.docs("v1");
+
+export const fetchGoogleDocsFiles = async (files: string) => {
+  for await (const documentId of files) {
+    console.log("\nDownloading document", documentId);
+    try {
+      const result = await docs.documents.get({
+        documentId,
+        auth: oauth2Client,
+      });
+      if (!result.data.title) throw new Error("Title not found");
+      await fs.writeFile(
+        join(".", `${result.data.title}.md`),
+        googleDocsToMarkdown(result.data)
+      );
+      console.log("Downloaded document", result.data.title);
+    } catch (error) {
+      console.log("Got an error", error);
+    }
+  }
+};
 
 export const googleDocsToMarkdown = (file: docs_v1.Schema$Document) => {
   let text = `---
